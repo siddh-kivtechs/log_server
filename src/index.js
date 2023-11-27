f25a15337e53832489728b73bfc6a8037461e677
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-28 02:01:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-const express = require("express");  
-const bodyParser = require("body-parser");  
+import express from 'express';  
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai";  
+import cors from 'cors';  
+import { createClient } from "@supabase/supabase-js";  
+import { ulid } from 'ulid';  
+import bodyParser from 'body-parser';  
+import helmet from 'helmet';  
+import morgan from 'morgan';  
+import dotenv from "dotenv/config";  
+  
 const app = express();  
 const PORT = process.env.PORT || 3000;  
+app.use(cors());  
+app.use(bodyParser.json());  
+app.use(morgan('combined'));  
+app.use(helmet());  
   
-// Add body parsing middleware  
-// app.use(bodyParser.json());  
-// app.use(bodyParser.urlencoded({ extended: true }));  
+
+const ulidgen=ulid();
+
+
+
   
-app.all("*", (req, res) => {  
-  res.send(req.body);  
-  console.log(req.body);  
-});  
+app.all("*", async (req, res) => {  
+  const data = req.body;  
+  const jsonString = JSON.stringify(data);  
+  const strippedStr = jsonString.replace(/`/g, '');  
+  
+
+
+    let dbdata={
+        created_at: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+        data:data
+    };
+console.log(dbdata);
+  res.send(data);
+
+  
+
+ 
+  
+}); 
+
+
   
 app.listen(PORT, () => {  
-  console.log(`API is listening on port ${PORT}`);  
+  console.log(`Server listening on port ${PORT}`);  
 });  
