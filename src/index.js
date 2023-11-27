fc4092e3b0726f0636ac2d0180740eb4a589bdfe
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-28 01:47:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");  
+const bodyParser = require("body-parser");  
 const app = express();  
 const PORT = process.env.PORT || 3000;  
   
 // Add body parsing middleware  
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true }));  
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));  
   
 app.all("*", (req, res) => {  
   res.send(req.body);  
