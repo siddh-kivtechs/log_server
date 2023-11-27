62b42de082480d89a3ead0de8feec8230305ce98
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-28 01:46:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-// In src/index.js
-const express = require("express");
-
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// *** REMOVE ***
-app.all("*", (req, res) => {
-  res.send(req.body);
-  console.log(req.body);
-});
-
-
-app.listen(PORT, () => {
-  console.log(`API is listening on port ${PORT}`);
-});
+const express = require("express");  
+const app = express();  
+const PORT = process.env.PORT || 3000;  
+  
+// Add body parsing middleware  
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
+  
+app.all("*", (req, res) => {  
+  res.send(req.body);  
+  console.log(req.body);  
+});  
+  
+app.listen(PORT, () => {  
+  console.log(`API is listening on port ${PORT}`);  
+});  
