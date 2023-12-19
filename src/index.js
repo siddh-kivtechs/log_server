2af1d0179d5be2c885a1e4fd2ec7015fc1abc999
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-12-19 15:13:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());  
 app.use(bodyParser.json());  
 app.use(morgan('combined'));  
-app.use(helmet());   
+app.use(helmet()); 
+//  Set & Initialize Supabase 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
   
 const ulidgen = ulid(); 
   
@@ -21,16 +25,29 @@ const ulidgen = ulid();
 app.all("*", async (req, res) => {  
   try {  
     const data = req.body;  
-    const logs = data.logs; // Access the "logs" array  
+    const logs = data.logs; 
   
-    console.log(logs); // Print the entire logs array  
+    console.log(logs); 
   
     let dbdata = {  
-      created_at: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),  
       data: data  
     };  
  
     res.send(data);  
+  //  Send the log to SUPABASE 
+
+       supabase  
+  .from('hooks')  
+  .insert([dbdata])  
+  .then(response => {  
+    console.log('Data sent to Supabase successfully:', response);  
+  })  
+  .catch(error => {  
+    console.error('Error sending data to Supabase:', error);  
+  });  
+
+
+   
   } catch (error) {  
     console.error(error);  
     res.status(500).send('Internal Server Error');  
